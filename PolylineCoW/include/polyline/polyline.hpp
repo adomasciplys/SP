61a76351107e1fa9188934043acd1d3f68f81dfc
Adomas Ciplys <aciply22@student.aau.dk>
2026-02-13 12:21:08 +0100

    Add PolylineCow added shared pointer stuff

--- PolylineCoW/include/polyline/polyline.hpp
+++ PolylineCoW/include/polyline/polyline.hpp
@@ -3,26 +3,32 @@
 #include <vector>
 #include <string>
 #include <sstream>
-
+#include <memory>
 
 namespace poly {
 
 struct Polyline
 {
-    void operator+=(const Point& new_point) { points.push_back(new_point); }
+    void operator+=(const Point& new_point)
+    {
+        if (points.use_count() > 1) {
+            points = std::make_shared<std::vector<Point>>(*points);
+        }
+        points->push_back(new_point);
+    }
 
-    explicit operator bool() const { return !points.empty(); }
+    explicit operator bool() const { return !points->empty(); }
 
 private:
-    std::vector<Point> points = {};
+    std::shared_ptr<std::vector<Point>> points = std::make_shared<std::vector<Point>>();
 
     friend std::ostream& operator<<(std::ostream& os, const Polyline& polyline)
     {
-        os << "Polyline@" << &polyline << "[";
-        for (std::size_t i = 0; i < polyline.points.size(); ++i) {
+        os << "Polyline@" << polyline.points << "[";
+        for (std::size_t i = 0; i < polyline.points->size(); ++i) {
             if (i > 0) {
                 os << ", ";
-                os << polyline.points[i];
+                os << polyline.points->at(i);
             }
         }
         os << "]";
