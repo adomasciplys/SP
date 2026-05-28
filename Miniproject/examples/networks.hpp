6a6a8e7abaea18e85f397a5b6cac1a6b29f3f930
Adomas Ciplys <aciply22@student.aau.dk>
2026-05-28 18:27:12 +0200

    added comments to network.hpp

--- Miniproject/examples/networks.hpp
+++ Miniproject/examples/networks.hpp
@@ -31,29 +31,29 @@ inline Vessel make_seihr(std::uint32_t N)
 {
     auto v = Vessel{"COVID19 SEIHR: " + std::to_string(N)};
 
-    const auto eps = 0.0009;
-    const auto I0 = static_cast<std::size_t>(std::round(eps * N));
-    const auto E0 = static_cast<std::size_t>(std::round(eps * N * 15));
-    const auto S0 = static_cast<std::size_t>(N) - I0 - E0;
-    const auto R0 = 2.4;
-    const auto alpha = 1.0 / 5.1;
-    const auto gamma = 1.0 / 3.1;
-    const auto beta = R0 * gamma;
-    const auto P_H = 0.9e-3;
-    const auto kappa = gamma * P_H * (1.0 - P_H);
-    const auto tau = 1.0 / 10.12;
+    const auto eps = 0.0009; // initial fraction of infectious
+    const auto I0 = static_cast<std::size_t>(std::round(eps * N)); // initial infectious
+    const auto E0 = static_cast<std::size_t>(std::round(eps * N * 15)); // initial exposed
+    const auto S0 = static_cast<std::size_t>(N) - I0 - E0; // initial susceptible
+    const auto R0 = 2.4; // initial basic reproductive number
+    const auto alpha = 1.0 / 5.1; // incubation rate (E -> I) ~5.1 days
+    const auto gamma = 1.0 / 3.1; // recovery rate (I -> R) ~3.1 days
+    const auto beta = R0 * gamma; // infection/generation rate (S+I -> E+I)
+    const auto P_H = 0.9e-3; // probability of hospitalization
+    const auto kappa = gamma * P_H * (1.0 - P_H); // hospitalization rate (I -> H)
+    const auto tau = 1.0 / 10.12; // removal rate in hospital (H -> R)
 
-    const auto S = v.add("S", S0);
-    const auto E = v.add("E", E0);
-    const auto I = v.add("I", I0);
-    const auto H = v.add("H", 0);
-    const auto R = v.add("R", 0);
+    const auto S = v.add("S", S0); // susceptible
+    const auto E = v.add("E", E0); // exposed
+    const auto I = v.add("I", I0); // infectious
+    const auto H = v.add("H", 0); // hospitalized
+    const auto R = v.add("R", 0); // removed/immune (recovered + dead)
 
-    v.add((S + I) >> beta / N >>= E + I);
-    v.add(E >> alpha >>= I);
-    v.add(I >> gamma >>= R);
-    v.add(I >> kappa >>= H);
-    v.add(H >> tau >>= R);
+    v.add((S + I) >> beta / N >>= E + I); // susceptible becomes exposed by infectious
+    v.add(E >> alpha >>= I); // exposed becomes infectious
+    v.add(I >> gamma >>= R); // infectious becomes removed
+    v.add(I >> kappa >>= H); // infectious becomes hospitalized
+    v.add(H >> tau >>= R); // hospitalized becomes removed
 
     return v;
 }
